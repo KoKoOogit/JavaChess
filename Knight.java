8811428baf991c6bd5af5ce44d146a9e0db9797d
Ko Ko Oo <kokooo@kokooo.local>
2024-01-28 12:11:43 -0800

    Knight Added

---
+++ Knight.java
@@ -0,0 +1,18 @@
+import java.lang.Math;
+
+public class Knight extends Piece {
+    
+    public Knight(int r, int file, Player player, String iconPath, Board board) {
+        super(r, file, player, iconPath, board);
+    }
+    @Override
+    public boolean isValidMove(int pRank, int pFile){
+        int diffFile = Math.abs(pFile - file);
+        int diffRank = Math.abs(pRank - rank);
+
+        if (diffFile == 2 && diffRank == 1 || diffFile == 1 && diffRank == 2){
+            return true;
+        }
+        return false; 
+    }
+}
--- Main.java
+++ Main.java
@@ -7,7 +7,9 @@ public class Main {
         ChessFrame cf = new ChessFrame(800);
         Player one = new Player();
         Piece p = new Pawn(0, 0, one, "./icons/w-pawn.png", cp.getBoard());
+        Piece k = new Knight(4, 4, one, "./icons/w-knight.png", cp.getBoard());
         cp.getBoard().getTile(0, 0).setPiece(p);
+        cp.getBoard().getTile(4, 4).setPiece(k);
         //this code should be at the end
 
         cf.setVisible(true);
